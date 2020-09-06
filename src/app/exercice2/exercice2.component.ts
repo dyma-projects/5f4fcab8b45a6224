aebcb18655e30580c25aa4cb57eaf58c99f14640
Theraen <quentin.barraud2@gmail.com>
2020-09-06 19:14:40 +0200

    Exercice 3

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice2.component.css']
 })
 export class Exercice2Component implements OnInit {
+
+  @ViewChild('v') public el: ElementRef;
   public valeur: string;
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
   }
